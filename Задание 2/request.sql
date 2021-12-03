230454920e2efc41571ca18459055327a175045e
Rodion <rodjaolast002@gmail.com>
2021-12-04 02:06:27 +0500

    add sql requests

--- Задание 2/request.sql
+++ Задание 2/request.sql
@@ -1,1 +1,20 @@
-SELECT FirstName, LastName FROM Students, Exams WHERE count(Result < 3) > 2
+--а)
+SELECT students.FirstName, students.LastName
+FROM students INNER JOIN Exams
+ON Students.StudentId = Exams.StudentId
+WHERE Exams.Result < 3
+GROUP BY Students.StudentId HAVING COUNT(Exams.ExamName) >= 2;
+
+--б)
+SELECT students.GroupName
+FROM (
+	SELECT students.StudentId
+    FROM students INNER JOIN Exams
+    ON Students.StudentId = Exams.StudentId
+    WHERE Exams.Result < 3
+    GROUP BY Students.StudentId HAVING COUNT(Exams.ExamName) >= 2
+) AS ten
+INNER JOIN students ON Students.StudentId = ten.StudentId
+GROUP BY students.GroupName HAVING COUNT(students.StudentId) > 2;
+
+
